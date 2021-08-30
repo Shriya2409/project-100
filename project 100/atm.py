a2bcf6cd879b212c84cdf54dbf886fdfa6345a61
Shriya2409 <72084377+Shriya2409@users.noreply.github.com>
2021-08-30 12:17:21 +0530

    Add files via upload

---
+++ project 100/atm.py
@@ -0,0 +1,9 @@
+class Atm(object):
+    def __init__ (self, user, cardNumber, pinNumber):
+        self.user=user
+        self.cardNumber=cardNumber
+        self.pinNumber=pinNumber
+    def CashWithdrawl(self):
+        print("Cash has been withdrawd")
+    def BalanceEnquiry(self):
+        print("Your balance is: ___")
